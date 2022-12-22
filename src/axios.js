3a1ddb5e24d7ce1e4833d23ae80cd8d8063f551a
ManeeshChikoti <96185794+ManeeshChikoti@users.noreply.github.com>
2022-12-22 17:17:03 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:" https://mstreamweb.herokuapp.com/api/"
+    baseURL:" https://mstreambackend.vercel.app/api/"
+  
 })
